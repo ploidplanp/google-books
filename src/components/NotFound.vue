6d5821a434b1aff3db7776692939b57c7efef61d
prd <waoekpr@gmail.com>
2021-03-17 22:25:27 +0700

    first commit

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>404</h1>
+        <p><i>O OH, THE PAGE YOU REQUEST WAS NOT FOUND :'(</i></p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<style scoped>
+h1 {
+    font-size: 15vw;
+    font-weight: bold;
+}
+</style>
